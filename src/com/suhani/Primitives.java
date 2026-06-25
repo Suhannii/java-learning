361afa5d463becc0c36b83234f7971d56227e3f5
Suhannii <suhanisinha006@example.com>
2026-06-25 13:28:00 +0530

    Add Primitives java file

---
+++ src/com/suhani/Primitives.java
@@ -0,0 +1,16 @@
+package com.suhani;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 's';
+        float marks = 94.25f;
+        double largeDecimalNumbers = 2184656466.1224;
+        long largeInteger = 232656210256595324L;
+        boolean check = true;
+
+
+        //String is not primitive
+
+    }
+}
