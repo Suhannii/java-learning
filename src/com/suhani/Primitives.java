19435bae82ed17e2c3e426dfb7b10dd1ca67ce40
Suhannii <suhanisinha006@gmail.com>
2026-06-25 13:44:33 +0530

    Update primitives comment

--- src/com/suhani/Primitives.java
+++ src/com/suhani/Primitives.java
@@ -1,14 +1,14 @@
 package com.suhani;
-// Learning Java primitives
+// Learning Java primitives!
 public class Primitives {
     public static void main(String[] args) {
+       //Primitives Data type
         int rollno = 64;
         char letter = 's';
         float marks = 94.25f;
         double largeDecimalNumbers = 2184656466.1224;
         long largeInteger = 232656210256595324L;
         boolean check = true;
-
 
         //String is not primitive
 
