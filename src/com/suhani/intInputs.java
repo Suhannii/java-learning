901f0af2812b231537e4a2509175eefca208ac16
Suhannii <suhanisinha006@gmail.com>
2026-06-28 20:26:34 +0530

    Fix variable output formatting in intInputs

--- src/com/suhani/intInputs.java
+++ src/com/suhani/intInputs.java
@@ -3,10 +3,14 @@ package com.suhani;
 import java.util.Scanner;
 
 public class intInputs {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter some input: ");
         int rollno = input.nextInt();
         System.out.println("Your roll number is " + rollno);
+
+        int a = 243_000_000;
+        System.out.println(a);
     }
 }
