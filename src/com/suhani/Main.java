19d39f9098494ea83830a6cda4c5ff8914036477
Suhannii <suhanisinha006@gmail.com>
2026-06-29 21:34:01 +0530

    Add Sum learning file

---
+++ src/com/suhani/Main.java
@@ -0,0 +1,10 @@
+package com.suhani;
+
+public class Main {
+   public static void main(String[] args)
+   {
+       System.out.println("Hello World!");
+       System.out.println("My name is Suhani");
+   }
+
+}
--- src/com/suhani/TypeCasting.java
+++ src/com/suhani/TypeCasting.java
@@ -1,4 +1,41 @@
 package com.suhani;
 
+import java.util.Scanner;
+
 public class TypeCasting {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Converting integer to float is known as type casting
+
+//        float num = input.nextFloat();
+//        int num = input.nestInt();
+//        System.out.println(num);
+
+        // Type Casting
+        int num = (int)(45.15f);
+        System.out.println(num);
+
+        // Automatic type promotion in expressions
+        int a = 257;
+        byte b = (byte)(a); // 257 % 256 = 1
+
+        System.out.println(b);
+
+//        byte c = 40;
+//        byte d = 50;
+//        byte e = 100;
+//        int f = (c * d) / e;
+//
+//        System.out.println(f);
+
+        int number = 'a';
+        System.out.println(number);
+
+        int number1 = 'A';
+        System.out.println(number1);
+
+
+
+    }
 }
