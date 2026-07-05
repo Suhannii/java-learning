0a64c14078cfce44544b0c239e76e01be00ff8bf
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-05 20:54:24 +0530

    Add CountNums.java to count specific digit occurrences in a number

---
+++ Conditional Loop/src/com/code/CountNums.java
@@ -0,0 +1,17 @@
+package com.code;
+
+public class CountNums {
+    static void main(String[] args) {
+        int n = 455365;
+
+        int count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 5) {
+                count++;
+            }
+            n = n / 10; // n/=10
+        }
+        System.out.println(count);
+    }
+}
