777b328f2abc4dc26e9d7fcf94cf430a36d9d734
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-07 21:43:46 +0530

    add day-of-week program using enhanced switch statement

--- Switch/src/com/code/Main.java
+++ Switch/src/com/code/Main.java
@@ -28,7 +28,19 @@ public class Main {
 
 
 
-        
+        System.out.print("Please enter a day number (1-7): ");
+        int day = in.nextInt();
+
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Invalid day number!");
+        }
         
     }
 }
