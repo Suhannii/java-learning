4b40486a2d2f30faa95d0825e1c338eeabe6c053
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-08 20:56:32 +0530

    dd NestedSwitch.java demonstrating nested switch statements

---
+++ Switch/src/com/code/NestedSwitch.java
@@ -0,0 +1,39 @@
+package com.code;
+
+import java.util.Scanner;
+
+public class NestedSwitch {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int empID = in.nextInt();
+        String department = in.next();
+
+
+        switch (empID) {
+            case 1:
+                System.out.println("Suhani Sinha");
+                break;
+            case  2:
+                System.out.println("Sneha Yadav");
+                break;
+            case  3:
+                System.out.println("Emp Number 3");
+                switch (department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "Management":
+                        System.out.println("Management Departmentt");
+                        break;
+                    default:
+                        System.out.println("No department Invalid");
+                }
+                break;
+            default:
+                System.out.println("Enter correct EmpID");
+        }
+
+       
+    }
+}
