464f954658431b0a15dee278f86e2ad6e1c324b1
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-08 20:51:48 +0530

    add weekday/weekend classification demonstrating switch fall-through

--- Switch/src/com/code/Main.java
+++ Switch/src/com/code/Main.java
@@ -7,40 +7,59 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Please enter fruit name: ");
-        String fruit = in.next();
+//        System.out.print("Please enter fruit name: ");
+//        String fruit = in.next();
+//
+//        if (fruit.equals("mango")) {
+//            System.out.println("King of fruit");
+//        }
+//
+//        if (fruit.equals("apple")) {
+//            System.out.println("a sweet red fruit");
+//        }
+//
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruits");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "orange" -> System.out.println("Round fruit");
+//            case "Grapes" -> System.out.println("Small fruit");
+//            default -> System.out.println("please enter a valid fruit");
+//        }
 
-        if (fruit.equals("mango")) {
-            System.out.println("King of fruit");
-        }
-
-        if (fruit.equals("apple")) {
-            System.out.println("a sweet red fruit");
-        }
-
-        switch (fruit) {
-            case "Mango" -> System.out.println("King of fruits");
-            case "Apple" -> System.out.println("A sweet red fruit");
-            case "orange" -> System.out.println("Round fruit");
-            case "Grapes" -> System.out.println("Small fruit");
-            default -> System.out.println("please enter a valid fruit");
-        }
 
 
-
-        System.out.print("Please enter a day number (1-7): ");
+//        System.out.print("Please enter a day number (1-7): ");
         int day = in.nextInt();
 
-        switch (day) {
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
-            default -> System.out.println("Invalid day number!");
-        }
+//        switch (day) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//            default -> System.out.println("Invalid day number!");
+//        }
+
+//        switch (day) {
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Weekdays");
+//                break;
+//            case 6:
+//            case 7:
+//                System.out.println("Weekend");
+//                break;
+//        }
+
+       
+
+
+
         
     }
 }
