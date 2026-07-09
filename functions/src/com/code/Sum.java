647aa96e0ce98e17a2a1bf18e7a480c97629dbf6
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-09 22:18:06 +0530

    implement basic sum method to demonstrate custom functions syntax

---
+++ functions/src/com/code/Sum.java
@@ -0,0 +1,35 @@
+package com.code;
+
+import java.util.Scanner;
+
+public class Sum {
+    static void main(String[] args) {
+
+        System.out.println(ans);
+
+}
+//        System.out.println("This will never execute");
+    }
+
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2 : ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+
+        /*
+             look in furture parts:-
+             access modifier (we'll look in OOP)
+             Only look this now:-
+             return_type name() {
+                  // body
+                  return statement;
+              }
+         */
+
+}
