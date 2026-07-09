6fb76c83f12b0a8b2984f1167a9417a2f0260b35
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-09 22:23:57 +0530

    add sum3 method to demonstrate passing parameters into functions

--- functions/src/com/code/Sum.java
+++ functions/src/com/code/Sum.java
@@ -7,8 +7,16 @@ public class Sum {
 
         System.out.println(ans);
         int ans = sum2();
+        int ans = sum3(20,30 );
 
 }
+
+// pass the value of numbers when you are calling the method in main()
+    static  int sum3(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+    
 
  // return the value
     static int sum2() {
