38d3b74b179d04e5a3a81bc2e074556f6ae4e27e
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-10 22:45:08 +0530

    add Greeting.java to demonstrate simple void methods without parameters

---
+++ functions/src/com/code/Greeting.java
@@ -0,0 +1,11 @@
+package com.code;
+
+public class Greeting {
+    static void main(StringExample[] args) {
+         greeting();
+    }
+
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+}
