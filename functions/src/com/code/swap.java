58272f892badd9867252f2a74a3ae6e950614731
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-11 21:55:51 +0530

    add swap.java demonstrating basic value swapping logic using a temporary variable

---
+++ functions/src/com/code/swap.java
@@ -0,0 +1,18 @@
+package com.code;
+
+public class swap {
+    static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // swap numbers code
+        int temp = a;
+        a = b;
+        b = temp;
+
+       
+    }
+
+    
+}
