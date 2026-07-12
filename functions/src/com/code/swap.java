35ae4b0450e0a75b5b9f95df068717f122814ebc
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-12 20:49:13 +0530

    update swap.java to demonstrate Java's pass-by-value mechanics

--- functions/src/com/code/swap.java
+++ functions/src/com/code/swap.java
@@ -7,12 +7,30 @@ public class swap {
         int b = 20;
 
         // swap numbers code
+        // int temp = a;
+        // a = b;
+        // b = temp;
+        
+        swap(a, b);
+
+        System.out.println(a + " " +  b);
+
+        String name = "Suhani Sinha";
+        changeName(name);
+        System.out.println(name);
+        
+    }
+    
+      private static void changeName(String name) {
+        name = "Sneha yadav";
+    }
+
+    static void swap(int a, int b) {
         int temp = a;
         a = b;
         b = temp;
-
-       
     }
+        
 
     
 }
