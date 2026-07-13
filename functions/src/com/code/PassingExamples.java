b0020f0831d08dd002b5baac35b55296e1704c6d
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-13 20:34:29 +0530

    add PassingExamples.java to demonstrate passing object references to methods

---
+++ functions/src/com/code/PassingExamples.java
@@ -0,0 +1,13 @@
+package com.code;
+
+public class PassingExamples {
+    static void main(String[] args) {
+        String name = "Suhani Sinha";
+        greet(name);
+    }
+
+    private static void greet(String naam) {
+        System.out.println(naam);
+    }
+
+}
