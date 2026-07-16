3c6ff48685a475101da28a4f74729bc289aa3869
Suhani Sinha <suhanisinha006@gmail.com>
2026-07-16 20:47:25 +0530

    larify variable scoping by updating method parameters in swap.java

--- functions/src/com/code/swap.java
+++ functions/src/com/code/swap.java
@@ -7,10 +7,10 @@ public class swap {
         int b = 20;
 
         // swap numbers code
-        // int temp = a;
-        // a = b;
-        // b = temp;
-        
+//        int temp = a;
+//        a = b;
+//        b = temp;
+
         swap(a, b);
 
         System.out.println(a + " " +  b);
@@ -18,19 +18,17 @@ public class swap {
         String name = "Suhani Sinha";
         changeName(name);
         System.out.println(name);
-        
-    }
-    
-      private static void changeName(String name) {
-        name = "Sneha yadav";
     }
 
-    static void swap(int a, int b) {
-        int temp = a;
-        a = b;
-        b = temp;
+    private static void changeName(String naam) {
+        naam = "Sneha yadav"; // creating a new object
     }
-        
 
-    
+    static void swap(int num1, int num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        // this change will only be valid in this function scope only
+    }
 }
